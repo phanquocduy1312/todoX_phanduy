9d4f69b157e0e8a783d8a36649e44966ca4ed427
phanquocduy1312 <phanquocduy1312@gmail.com>
2025-12-25 23:30:42 +0700

    chuan bi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,24 @@ import dotenv from 'dotenv';
 import cors from 'cors'
 dotenv.config()
 import { connectDB } from './config/db.js';
-
+import path from 'path'
+const __dirname=path.resolve();
 const app=express();
 // middlewares
 app.use(express.json())
+if(process.env.NODE_ENV!=='production'){
 app.use(cors({origin:'http://localhost:5173'}))
+}
+
 app.use('/api/tasks',router);
+if(process.env.NODE_ENV=='production'){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+
+})
+}
 connectDB().then(()=>{
 app.listen(process.env.PORT,()=>{
     console.log('server port:5001 is running');
